171fd0de880747068c4e6484f1dc99c3c4d8ee16
손유리 <yu_ri_son@naver.com>
2026-02-10 15:01:12 +0900

    feat: added font

--- src/ui/components/MainPage/Navbar.tsx
+++ src/ui/components/MainPage/Navbar.tsx
@@ -4,10 +4,10 @@ export default function Navbar() {
   return (
     <nav className='bg-zinc-900 p-6 pt-18 pl-10 mt-24 rounded-tr-4xl' style={{ height: 'calc(100% - 160px)' }}>
       <ul className='text-white flex flex-col gap-6'>
-        <li>
+        <li key="nav-search">
           <Link to='/search'>Search</Link>
         </li>
-        <li>
+        <li key="nav-my">
           <Link to='/my'>My Seotorage</Link>
         </li>
       </ul>
