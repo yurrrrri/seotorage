bf9cb1d600f4e64807b7bb22ba635f3f39c60cb7
손유리 <yu_ri_son@naver.com>
2026-02-11 17:39:05 +0900

    feat: created Input component

--- src/ui/pages/SearchPage.tsx
+++ src/ui/pages/SearchPage.tsx
@@ -1,9 +1,10 @@
 import Button from "@component/Button";
+import Input from "@component/Input";
 
 export default function SearchPage() {
   return <div className="flex flex-col gap-4">
     <div className="flex flex-row">
-      <input className="bg-zinc-200 h-8 p-2 rounded-sm mr-1 grow border-solid border-1 border-zinc-300" />
+      <Input />
       <Button >Search</Button>
     </div>
     <span>This is book search page.</span>
