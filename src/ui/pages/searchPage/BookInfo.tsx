76764e2be074c899cda6022ec1a0b893559f7d13
손유리 <yu_ri_son@naver.com>
2026-02-24 10:54:43 +0900

    style: changed default font

--- src/ui/pages/searchPage/BookInfo.tsx
+++ src/ui/pages/searchPage/BookInfo.tsx
@@ -6,12 +6,12 @@ export function BookInfo({ book }) {
   return (
     <div className="flex flex-row p-2 min-w-42 rounded-sm shadow-xs/20 border border-zinc-200 cursor-pointer bg-white hover:inset-shadow-sm/30"
       onClick={() => navigate(`/write/${book.id}`)}
-      style={{ fontSize: 13, lineHeight: 1.2 }}>
-      <img src={book.coverImage} style={{ minWidth: 80, height: 120 }} />
+      style={{ fontSize: 13, lineHeight: 1.25 }}>
+      <img src={book.coverImage} className="rounded-md" style={{ minWidth: 80, height: 120 }} />
       <div className="flex flex-col w-full">
-        <h1 className="border-b border-indigo-900 ml-3 py-2" style={{ fontWeight: 600, fontSize: 15 }}>{book.title}</h1>
+        <h1 className="border-b border-indigo-900 ml-3 py-1" style={{ fontWeight: 600, fontSize: 14.5 }}>{book.title}</h1>
         <p className="text-zinc-500 ml-3 mt-4">{book.author.substring(0, book.author.indexOf("("))}</p>
-        <p className="text-zinc-700 ml-3" style={{ lineHeight: 1.3, textAlign: 'justify' }}>{book.publisher}</p>
+        <p className="text-zinc-700 ml-3" style={{ marginTop: 1 }}>{book.publisher}</p>
       </div>
     </div>
   )
--- src/ui/pages/writePage/BookDetail.tsx
+++ src/ui/pages/writePage/BookDetail.tsx
@@ -3,19 +3,19 @@ import dayjs from "dayjs";
 export function BookDetail({ book }) {
   return (
     <div className='px-3 py-4 rounded-sm shadow-sm/20 bg-white border border-zinc-200'
-      style={{ lineHeight: 1.2 }}>
+      style={{ lineHeight: 1.2, fontSize: 13 }}>
       <div className="flex">
         <img src={book.coverImage} style={{ width: 100, height: 130 }} />
         <div className='ml-2'>
-          <h1 className="pb-2 border-b border-indigo-950"
-            style={{ fontWeight: 600, fontSize: 17 }}>{book.title}</h1>
-          <div className='mt-2 text-zinc-600'>
+          <h1 className="pb-1 border-b border-indigo-950"
+            style={{ fontWeight: 600, fontSize: 15 }}>{book.title}</h1>
+          <div className='mt-3 text-zinc-600'>
             <p>{book.author}</p>
             <p>{book.publisher} | {dayjs(book.pubDate).format("YYYY.MM.DD")}</p>
           </div>
         </div>
       </div>
-      <p className='pt-4'>{book.description}</p>
+      <p className='pt-4' style={{ lineHeight: 1.3 }}>{book.description}</p>
     </div>
   )
 }
